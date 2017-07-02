b86c3cb1126707618d8abe4979238c80f1ffc126
ted <tedhoryczuniv@outlook.com>
2017-07-02 11:39:22 -0400

    start

---
+++ app/src/main/java/weatherapp/kotlin/com/weatherapp/JavaFile.java
@@ -0,0 +1,8 @@
+package weatherapp.kotlin.com.weatherapp;
+
+/**
+ * Created by ted on 7/1/17.
+ */
+
+public class JavaFile {
+}
---
+++ app/src/main/java/weatherapp/kotlin/com/weatherapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package weatherapp.kotlin.com.weatherapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val name = "Ted"
+    }
+}
