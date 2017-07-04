1aa3fce9e4b72948fc255a4263a5ee9ef45e9962
ted <tedhoryczuniv@outlook.com>
2017-07-04 10:42:13 -0400

    populateViewTemps

--- app/src/main/java/weatherapp/kotlin/com/weatherapp/MainActivity.kt
+++ app/src/main/java/weatherapp/kotlin/com/weatherapp/MainActivity.kt
@@ -9,15 +9,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        showGreeting("hello there!")
-        val answer = sum(1,5)
-        println("answer: $answer")
+        val main = Main()
+        main.temp = 90
+        main.maxTemp = 100
+        main.minTemp = 20
+        val weather = Weather()
+        weather.main = main
+
+        presentData(main)
+
     }
 
-    fun sum(num1: Int, num2: Int): Int{
-      return num1 + num2
-    }
-    fun showGreeting(text: String){
-       println(text)
+    private fun  presentData(main: Main) {
+        currentTemp.text = main.temp.toString()
+        lowTemp.text = main.minTemp.toString()
+        highTemp.text = main.maxTemp.toString()
+
     }
 }
